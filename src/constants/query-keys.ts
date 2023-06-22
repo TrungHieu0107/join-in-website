6c4d759983331d0a4da460bf185b676f8fe6eebc
Trung Hiếu <ntrunghieu1701@gmail.com>
2023-06-22 20:46:54 +0700

    Update base url

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,5 +1,5 @@
 export const QueryKeys = {
-  BASE_LOCAL: 'http://apijoinin1-dev.eba-wpfq6kpu.ap-southeast-1.elasticbeanstalk.com',
+  BASE_LOCAL: 'https://apijoinin20230622182026.azurewebsites.net',
   GET_WORK_LIST: 'GET_WORK_LIST',
   EMAIL_REGEX:
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
