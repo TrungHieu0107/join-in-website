afdc7f029c71b540426271c5778b48e7aa6097fb
Trung Hiếu <ntrunghieu1701@gmail.com>
2023-06-23 09:27:23 +0700

    Change URL backend

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,5 +1,5 @@
 export const QueryKeys = {
-  BASE_LOCAL: 'https://apijoinin20230622182026.azurewebsites.net',
+  BASE_LOCAL: 'https://apijoinin20230623091433.azurewebsites.net',
   GET_WORK_LIST: 'GET_WORK_LIST',
   EMAIL_REGEX:
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
