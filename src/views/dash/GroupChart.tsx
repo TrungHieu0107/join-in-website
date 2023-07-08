935e65e571210ab67a214d89e28c9ebce2534e60
Thanh Huy <88583984+huylt-orca@users.noreply.github.com>
2023-07-08 21:07:30 +0700

    fix total group

--- src/views/dash/GroupChart.tsx
+++ src/views/dash/GroupChart.tsx
@@ -82,7 +82,7 @@ const GroupChart = (props: { listDataGroup: number[]}) => {
   return (
     <Card>
       <CardHeader
-        title={'Group Statistics' + totalGroup}
+        title={'Group Statistics (total group: ' + totalGroup + ')'}
         titleTypographyProps={{
           sx: { lineHeight: '2rem !important', letterSpacing: '0.15px !important' }
         }}
